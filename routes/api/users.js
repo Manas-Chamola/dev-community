6aba7e4dcec53ec3fed983bf38d0042109de9e16
Manas <chamolamanas19@gmail.com>
2020-06-12 23:16:01 +0530

    Created different routes

---
+++ routes/api/users.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+//@route   GET api/users
+//@desc    Test Route
+//@access  Public
+router.get("/", (req, res) => res.send("User Route"));
+
+module.exports = router;
