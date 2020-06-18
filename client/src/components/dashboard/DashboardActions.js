3b44fd93dfe8ffefe314a71095c91b6e96b610e4
Manas <chamolamanas19@gmail.com>
2020-06-18 22:38:48 +0530

    Bugs fixed

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -7,10 +7,10 @@ const DashboardActions = () => {
       <Link to="/edit-profile" className="btn btn-light">
         <i className="fas fa-user-circle text-primary"></i> Edit Profile
       </Link>
-      <Link to="add-experience" className="btn btn-light">
+      <Link to="/add-experience" className="btn btn-light">
         <i className="fab fa-black-tie text-primary"></i> Add Experience
       </Link>
-      <Link to="add-education" className="btn btn-light">
+      <Link to="/add-education" className="btn btn-light">
         <i className="fas fa-graduation-cap text-primary"></i> Add Education
       </Link>
     </div>
--- client/src/components/profile-form/EditProfile.js
+++ client/src/components/profile-form/EditProfile.js
@@ -45,7 +45,7 @@ const EditProfile = ({
       youtube: loading || !profile.social ? "" : profile.social.youtube,
       instagram: loading || !profile.social ? "" : profile.social.instagram,
     });
-  }, [loading, getCurrentProfile, profile]);
+  }, [loading]);
 
   const {
     company,
